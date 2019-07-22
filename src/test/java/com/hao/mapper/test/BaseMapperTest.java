4b51876bda54aec21424aea8db8323013968d569
haowenlong <haowenlong@yeah.net>
2019-07-22 15:28:00 +0800

    feat:测试一级缓存，二级缓存

--- src/test/java/com/hao/mapper/test/BaseMapperTest.java
+++ src/test/java/com/hao/mapper/test/BaseMapperTest.java
@@ -1,5 +1,59 @@
 package com.hao.mapper.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.hao.model.user.User;
+
 public class BaseMapperTest {
+
+	protected SqlSessionFactory sqlSessionFactory;
+
+	// 测试前执行
+	@Before
+	public void setUp() throws IOException {
+		// 配置文件
+		String resource = "mybatis/mybatis-config.xml";
+		// 的到配置文件流
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		// 创建会话工厂
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			String resource = "/mybatis/mybatis-config.xml";
+			Resource r = new ClassPathResource(resource);
+			InputStream inputStream = r.getInputStream();
+			SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+
+			SqlSessionFactory factory = builder.build(inputStream);
+
+			SqlSession sqlSession = factory.openSession();
+
+			User record = new User();
+			record.setAge(1);
+			record.setSalary(new BigDecimal(200000.0));
+
+			sqlSession.insert("com.hao.mapper.user.UserMapper.insert", record);
+
+			sqlSession.commit();
+
+			sqlSession.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- src/test/java/com/hao/mapper/test/UserMapperTest.java
+++ src/test/java/com/hao/mapper/test/UserMapperTest.java
@@ -1,45 +1,49 @@
 package com.hao.mapper.test;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.math.BigDecimal;
 
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.junit.Test;
 
+import com.hao.mapper.user.UserMapper;
 import com.hao.model.user.User;
 
-public class UserMapperTest {
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.json.JSONUtil;
+
+public class UserMapperTest extends BaseMapperTest{
 	
-	
-	public static void main(String[] args) {
+	@Test
+	public void testInsert() {
 		
-		try {
-			String resource = "/mybatis/mybatis-config.xml";
-			 Resource r = new ClassPathResource(resource);
-			InputStream inputStream = r.getInputStream();
-			SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-			
-			SqlSessionFactory factory = builder.build(inputStream);
-			
-			SqlSession sqlSession = factory.openSession();
-			
-			User record = new User();
-			record.setAge(1);
-			record.setSalary(new BigDecimal(200000.0));
-			
-			
-			sqlSession.insert("com.hao.mapper.user.UserMapper.insert",record);
-			sqlSession.commit();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		
+		User record = new User();
+		record.setUserName("程咬金");
+		record.setAge(18);
+		record.setBirthDate(DateUtil.parse("1990-08-20"));
+		record.setSalary(new BigDecimal("20000"));
+		record.setSex("Y");
+		record.setStatus(1);
+		userMapper.insert(record);
 		
 	}
 	
+	@Test
+	public void testSelect() {
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		
+		User user = userMapper.selectByPrimaryKey(6l);
+		System.out.println(JSONUtil.toJsonPrettyStr(user));
+		//手动清空一级缓存
+		sqlSession.clearCache();
+		
+		User user2 = userMapper.selectByPrimaryKey(6l);
+		System.out.println(JSONUtil.toJsonPrettyStr(user2));
+		
+	}
 	
 }
